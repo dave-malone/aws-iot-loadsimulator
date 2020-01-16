3a5dffa5fb9ec54d0dcff5da5b7ae2b762f01480
Aaron Molitor <ammolito@amazon.com>
2020-01-16 15:28:17 -0600

    fix packaging of worker lambda (certs dir)

--- scripts/build-worker-lambda.sh
+++ scripts/build-worker-lambda.sh
@@ -6,5 +6,8 @@ rm -f "${PROJECT_DIR}"/build/worker-handler*
 
 GOOS=linux go build -o "${PROJECT_DIR}"/build/worker-handler "${PROJECT_DIR}"/cmd/lambda/worker/main.go
 pushd "${PROJECT_DIR}"/build || exit 128
-zip worker-handler.zip worker-handler "${PROJECT_DIR}"/certs/*
+zip -r worker-handler.zip worker-handler
+pushd "${PROJECT_DIR}" || exit 128
+zip -ur "${PROJECT_DIR}"/build/worker-handler.zip certs
+popd || exit 128
 popd || exit 128
